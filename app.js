3eb723f5ca1d2089a64bc1b281191661268f7c3b
nikitayershov <nikita.yershov@gmail.com>
2022-04-18 17:27:52 +0300

    selection of slide added

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin (activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide')
+    
+    slides[activeSlide].classList.add('active')
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
-    })
-}
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
 
-function  clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
+    function  clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
+slidesPlugin(1)
+
 
 // function clearActiveClasses() {
 //     for (const slide of slides) {
